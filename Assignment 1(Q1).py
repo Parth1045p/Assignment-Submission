2266500592de7144e899144d0e03716f3f6d0e68
Parth1045p <56487268+Parth1045p@users.noreply.github.com>
2021-02-06 19:35:02 +0530

    Assignment1 -Q1 submitted

---
+++ Assignment 1(Q1).py
@@ -0,0 +1,6 @@
+num1=float(input("Enter the 1st no:"))
+num2=float(input("Enter the 1st no:"))
+print("Addition=",num1+num2)
+print("Subtraction=",num1-num2)
+print("Multiplication=",num1*num2)
+print("Division=",num1/num2)
